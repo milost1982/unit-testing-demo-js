32697c73386c213fd002dab649db5a6d481859cd
Milos Tanasijevic <milos.tns@gmail.com>
2021-06-16 16:34:04 +0200

    Add expect() method with toBe() assertion

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -3,13 +3,23 @@ const calc = require('../src/calculator')
 // test addition
 let result = calc.add(2,5)
 let expected = 7
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
-}
+expect(result).toBe(expected)
 
 // test substraction
 result = calc.sub(11,3)
 expected = 8
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+expect(result).toBe(expected)
+
+function expect(actual) {
+  return {
+    toBe (expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`)
+      }
+    },
+    // toBeGreaterThan(expected) {},
+    // toBeLessThan(expected) {},
+    // toEqual(expected) {},
+    // toBeNull() {}
+  }
 }
